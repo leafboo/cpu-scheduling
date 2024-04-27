3846a7195d8104f620ff63187947ce944088c957
Jan romel <janjancaranguian@gmail.com>
2024-04-27 20:28:28 +0800

    Style <HomePage> a bit and add state for page numbers

--- cpu-scheduling/src/App.tsx
+++ cpu-scheduling/src/App.tsx
@@ -1,12 +1,25 @@
-import './App.css'
+import React from 'react'
+import HomePage from './pages/HomePage/HomePage.tsx'
 import MainPage from './pages/MainPage/MainPage.tsx'
+import AppCSS from './App.module.css'
 
 export default function App() {
+  const [pageNumber, setPageNumber] = React.useState(1)
+
+
+  function changePage() {
+    
+  }
 
   return (
-    <>
-      <MainPage />
-    </>
+    <div className={AppCSS['app-container']}>
+      {
+        pageNumber === 2 ? (
+          <MainPage />
+        ) : (<HomePage changePage={changePage} />)
+      }
+      
+    </div>
   )
 }
 
--- cpu-scheduling/src/pages/HomePage/HomePage.tsx
+++ cpu-scheduling/src/pages/HomePage/HomePage.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import HomePageCSS from './HomePage.module.css'
 
-export default function HomePage() {
+export default function HomePage(props) {
   return (
-    <>
+    <div className={HomePageCSS['home-page-container']}>
+      <div> CPU Scheduling</div>
+     
+      <button className={HomePageCSS['start-button']} onClick={props.changePage}>Start</button>
          
-    </>
+    </div>
   )
 }
