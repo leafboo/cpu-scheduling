5741a9319cb43cee5f62d184730cb6b65ef2ac99
Jan romel <janjancaranguian@gmail.com>
2024-05-12 14:46:10 +0800

    Disable realtime table update

--- cpu-scheduling/src/buttons/Compute/Compute.tsx
+++ cpu-scheduling/src/buttons/Compute/Compute.tsx
@@ -4,15 +4,18 @@ import ComputeCSS from './Compute.module.css'
 
 export default function Compute(props: any) {
   const [isButtonPressed, setIsButtonPressed] = React.useState(false)
+  const [tempResults, setTempResults] = React.useState()
+ 
 
   return (
     <>
       <button className={ComputeCSS['compute-button']} onClick={() => {
         setIsButtonPressed(true)
+        setTempResults(props.results)
 
       }}>Compute</button>
       {
-        isButtonPressed ? <Table results={props.results} /> : null
+        isButtonPressed ? <Table results={tempResults} /> : null
       }
       
     </>
