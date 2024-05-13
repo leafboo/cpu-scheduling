0353278a18c1da3f2a1f8c7a5f63ed4c4dc0d7ff
Jan romel <janjancaranguian@gmail.com>
2024-05-13 12:04:05 +0800

    Make the completion times appear under the <GanttChart>

--- cpu-scheduling/src/output/ganntChart/GanttChart.tsx
+++ cpu-scheduling/src/output/ganntChart/GanttChart.tsx
@@ -5,11 +5,22 @@ import GanttChartCSS from './GanttChart.module.css'
 export default function GanttChart(props: any) {
   const ProcessElement = props.results['Process'].map( (process: number) => 
     <Process processNumber={process}/>
+  );
+  const newResults = [0, ...props.results['Completion Time']];
+  const CompletionTimeElement = newResults.map((ct) => 
+    <span>{ct}</span>
   )
+
   
   return (
-    <div className={GanttChartCSS['gantt-chart-container']}>
-      { ProcessElement }
-    </div>
+    <>
+      <div className={GanttChartCSS['gantt-chart-container']}>
+        { ProcessElement }
+      </div>
+      <div className={GanttChartCSS['completion-times']}>
+        {CompletionTimeElement}
+      </div>
+    </>
+    
   )
 } 
